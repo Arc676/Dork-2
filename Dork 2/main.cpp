cdb5e08d575c49b4d501f1b1debda14ec3ddaab0
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-17 17:09:30 +0800

    Created Xcode project

---
+++ Dork 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dork 2
+//
+//  Created by Alessandro Vinciguerra on 17/11/2017.
+//  Copyright © 2017 Arc676. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
