abda7c4a5553fb9772d2ae344bbbdda33bc166b0
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-17 17:22:35 +0800

    Preparing for building with Orx

---
+++ Dork 2/StandAlone.cpp
@@ -0,0 +1,101 @@
+//
+//  StandAlone.cpp
+//  Dork 2
+//
+//  Created by Alessandro Vinciguerra on 17/11/2017.
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+#include "StandAlone.h"
+
+StandAlone* StandAlone::Instance() {
+	if (m_Instance != nullptr) {
+		return m_Instance;
+	}
+	m_Instance = new StandAlone();
+	return m_Instance;
+}
+
+StandAlone::StandAlone() {}
+
+orxSTATUS orxFASTCALL StandAlone::Init() {
+//	orxViewport_CreateFromConfig("Viewport");
+//	orxConfig_Load("StaticScene.ini");
+//	orxObject_CreateFromConfig("Scene");
+//
+//	orxConfig_Load("Entities.ini");
+//
+//	player = new Player();
+//	environment = new Environment();
+//
+//	orxConfig_Load("UI.ini");
+//	scoreLabel = orxObject_CreateFromConfig("ScoreLabel");
+//
+//	orxConfig_Load("Items.ini");
+//
+//	orxCLOCK* upClock = orxClock_FindFirst(-1.0f, orxCLOCK_TYPE_CORE);
+//	orxClock_Register(upClock, Update, orxNULL, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
+//
+//	orxEvent_AddHandler(orxEVENT_TYPE_PHYSICS, StandAlone::EventHandler);
+//	orxEvent_AddHandler(orxEVENT_TYPE_INPUT, StandAlone::EventHandler);
+
+	return orxSTATUS_SUCCESS;
+}
+
+orxSTATUS orxFASTCALL StandAlone::Run() {
+	return orxSTATUS_SUCCESS;
+}
+
+void orxFASTCALL StandAlone::Exit() {
+	return;
+}
+
+orxOBJECT* StandAlone::GetObjectByName(orxSTRING objName) {
+	for (orxOBJECT* obj = orxOBJECT(orxStructure_GetFirst(orxSTRUCTURE_ID_OBJECT));
+		 obj != orxNULL;
+		 obj = orxOBJECT(orxStructure_GetNext(obj))) {
+		if (orxString_Compare(orxObject_GetName(obj), objName) == 0) {
+			return obj;
+		}
+	}
+	return orxNULL;
+}
+
+void orxFASTCALL StandAlone::Update(const orxCLOCK_INFO* clockInfo, void* context) {
+	//
+}
+
+//orxVECTOR orxFASTCALL StandAlone::GetMouseWorldPosition() {
+//	orxVECTOR worldpos, screenpos;
+//
+//	orxRender_GetWorldPosition(orxMouse_GetPosition(&screenpos), orxNULL, &worldpos);
+//	worldpos.fZ = 0;
+//
+//	return worldpos;
+//}
+
+orxSTATUS orxFASTCALL StandAlone::EventHandler(const orxEVENT* currentEvent) {
+	switch(currentEvent->eType) {
+		case orxEVENT_TYPE_INPUT:
+			break;
+		case orxEVENT_TYPE_PHYSICS:
+			break;
+		default:
+			break;
+	}
+
+	return orxSTATUS_SUCCESS;
+}
