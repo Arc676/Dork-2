a381191a170477ef21de5c7a1b77c9f7b4d7bf24
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-20 20:36:43 +0800

    Defined predefined weapons

--- Dork 2/Items/Weapon.cpp
+++ Dork 2/Items/Weapon.cpp
@@ -21,6 +21,30 @@
 
 #include "Weapon.h"
 
+Weapon* Weapon::sword = new Weapon(
+	SWORD, 0.4, 0.4, 0, 700
+);
+Weapon* Weapon::club = new Weapon(
+	CLUB, 0.15, 0.15, -0.05, 100
+);
+Weapon* Weapon::scythe = new Weapon(
+	SCYTHE, 0.5, 0.3, -0.05, 1000
+);
+Weapon* Weapon::sickle = new Weapon(
+	SICKLE, 0.4, 0.2, 0, 400
+);
+Weapon* Weapon::axe = new Weapon(
+	AXE, 0.3, 0.1, -0.05, 600
+);
+Weapon* Weapon::dagger = new Weapon(
+	DAGGER, 0.3, 0.15, 0.05, 200
+);
+Weapon* Weapon::lance = new Weapon(
+	LANCE, 0.4, 0.1, -0.1, 700
+);
+
+Weapon::Weapon(WeaponType weaponType, double str, double def, double speed, int price) : weapon(weaponType), strMod(str), defMod(def), speedMod(speed), price(price) {}
+
 std::string Weapon::getWeaponName(WeaponType type) {
 	switch (type) {
 		case SWORD:
@@ -62,8 +86,6 @@ EntityType Weapon::getTypeForWeapon(WeaponType weaponType) {
 			return NOTYPE;
 	}
 }
-
-Weapon::Weapon(WeaponType weaponType, double str, double def, double speed, int price) : weapon(weaponType), strMod(str), defMod(def), speedMod(speed), price(price) {}
 
 Weapon* Weapon::copy() {
 	return new Weapon(weapon, strMod, defMod, speedMod, price);
