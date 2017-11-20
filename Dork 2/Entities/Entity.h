1551161681baeb0914ecc99ffd49f2f8282000d2
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-20 10:31:29 +0800

    Started working on entities

---
+++ Dork 2/Entities/Entity.h
@@ -0,0 +1,31 @@
+//
+//  Entity.hpp
+//  Dork 2
+//
+//  Created by Alessandro Vinciguerra on 20/11/2017.
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+#ifndef Entity_h
+#define Entity_h
+
+#include "orx.h"
+
+class Entity {
+public:
+};
+
+#endif
