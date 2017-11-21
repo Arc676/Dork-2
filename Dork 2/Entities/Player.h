96de1a76eac693df38bc328c2cb22b626057ef8e
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-21 14:10:39 +0800

    Started creating player

---
+++ Dork 2/Entities/Player.h
@@ -0,0 +1,36 @@
+//
+//  Player.hpp
+//  Dork 2
+//
+//  Created by Alessandro Vinciguerra on 21/11/2017.
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+#ifndef Player_h
+#define Player_h
+
+#include <map>
+
+#include "Entity.h"
+
+class Player : public Entity {
+	std::map<PotionType, int> ownedPotions;
+	std::string name;
+public:
+	virtual std::string getName();
+};
+
+#endif
