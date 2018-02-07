2ebe1c090518051f4b454215cad3a5a9b3c6541a
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-02-07 10:23:50 +0800

    Updated StatViewer to use Level

--- Dork 2/Scenes/StatViewer.cpp
+++ Dork 2/Scenes/StatViewer.cpp
@@ -90,7 +90,7 @@ void StatViewer::reloadData() {
 	orxString_Print(text, "Gold: %d", entity->getGold());
 	orxObject_SetTextString(labelGold, text);
 
-	orxString_Print(text, "Level: %f", entity->getLevel());
+	orxString_Print(text, "Level %d; %d XP", entity->getLevel().getLevel(), entity->getLevel().getXP());
 	orxObject_SetTextString(labelLvl, text);
 
 	orxString_Print(text, "Weapon: %s", entity->getWeapon()->getName());
