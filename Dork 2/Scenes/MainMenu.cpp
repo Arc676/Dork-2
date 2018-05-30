11efd75ca52fba09623b8e56ea266341b9cbc376
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-05-30 18:50:48 +0800

    Converted MainMenu buttons to text

--- Dork 2/Scenes/MainMenu.cpp
+++ Dork 2/Scenes/MainMenu.cpp
@@ -28,7 +28,7 @@ void MainMenu::activate() {
 	orxKeyboard_ClearBuffer();
 
 	orxVECTOR pos = Scene::createVector(-1216, 1592, 0);
-	orxObject_SetTextString(nameField, "Type your alphanumeric name");
+	orxObject_SetTextString(nameField, "Type alphanumeric name");
 	orxObject_SetPosition(nameField, &pos);
 
 	pos = Scene::createVector(-1125, 1652, 0);
